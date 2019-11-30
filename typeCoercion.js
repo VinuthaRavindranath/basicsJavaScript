1be79f9bba6efd6fcd65ce096a88b9fd3989faaa
Vinutha Ravindra <vinutharavindra@Vinutha-Ravindras-MacBook-Pro-3.local>
2019-11-30 17:41:28 +0530

    typeCoercion

---
+++ typeCoercion.js
@@ -0,0 +1,7 @@
+var firstName = "Vinutha";
+var lastName = "Ravindra";
+var dob = 27;
+console.log(firstName+ ' ' + lastName + 'Date of birth is' + dob);
+console.log(typeof(firstName));
+console.log(typeof(lastName));
+console.log(typeof(dob));
